3bdaa742240721c8ea1737a92e6471d00f8da799
shiuan <shiuan@Ubuntu.myguest.virtualbox.org>
2024-06-10 23:31:23 +0800

    Add finder.sh and update finder-test.sh to use writer executable

---
+++ finder-app/finder.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+writefile=$1
+writestr=$2
+
+if [ -z "$writefile" ] || [ -z "$writestr" ]; then
+    echo "Error: Two arguments are required. Usage: $0 <directory> <search_string>"
+    exit 1
+fi
+
+if [ ! -d "$writefile" ]; then
+    echo "Error: The specified directory '$writefile' does not exist."
+    exit 1
+fi
+
+num_files=$(find "$writefile" -type f | wc -l)
+num_matching_lines=$(grep -r "$writestr" "$writefile" | wc -l)
+
+echo "The number of files are $num_files and the number of matching lines are $num_matching_lines"
