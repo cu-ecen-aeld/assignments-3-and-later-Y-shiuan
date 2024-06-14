2f921e9882e50eb303c4b3b57fa401306e7a9d76
shiuan <shiuan@Ubuntu.myguest.virtualbox.org>
2024-06-15 01:56:20 +0800

    Update finder-test.sh for assignment 4

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -2,17 +2,13 @@
 # Tester script for assignment 1 and assignment 2
 # Author: Siddhant Jajoo
 
-# 确保在 finder-app 目录中编译
-make clean
-make
-
 set -e
 set -u
 
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -36,16 +32,12 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=$(cat conf/assignment.txt)
+assignment=$(cat /etc/finder-app/conf/assignment.txt)
 
 if [ "$assignment" != "assignment1" ]
 then
     mkdir -p "$WRITEDIR"
 
-    # The WRITEDIR is in quotes because if the directory path consists of spaces, 
-    # then variable substitution will consider it as multiple argument.
-    # The quotes signify that the entire string in WRITEDIR is a single string.
-    # This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
     if [ -d "$WRITEDIR" ]
     then
         echo "$WRITEDIR created"
@@ -56,10 +48,13 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-    ./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+    /usr/bin/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(/usr/bin/finder.sh "$WRITEDIR" "$WRITESTR")
+
+# Write the result to /tmp/assignment4-result.txt
+echo "$OUTPUTSTRING" > /tmp/assignment4-result.txt
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
